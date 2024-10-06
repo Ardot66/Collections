c7f1db9afcc938f0751103cef6c87b763c55e944
Gabriel Clay <gabrielwclay@gmail.com>
2024-10-06 16:13:15 +1100

    Removed List.c and related code in favour of CArray.c

--- Source/Collections.h
+++ Source/Collections.h
@@ -41,7 +41,6 @@ struct DStack
     size_t RemovedCount;
 };
 
-
 void *CArrayGetElement(const void *array, const size_t length, const size_t offset, const size_t elementSize, const size_t index);
 void CArraySet(void *array, const size_t length, const size_t offset, const size_t elementSize, const size_t index, const void *element);
 void CArrayGet(const void *array, const size_t length, const size_t offset, const size_t elementSize, const size_t index, void *elementDest);
@@ -49,15 +48,5 @@ void CArrayResizeElements(void *array, const size_t count, const size_t length, 
 int CArrayResize(void **array, const size_t count, size_t *length, size_t *arrayOffset, const size_t elementSize, const size_t newLength);
 void CArrayInsert(void *array, size_t *count, const size_t length, size_t *offset, const size_t elementSize, const size_t index, const void *element);
 void CArrayRemove(void *array, size_t *count, const size_t length, size_t *offset, const size_t elementSize, const size_t index);
-
-int ListCreate(const size_t elementSize, const size_t startingLength, List *listDest);
-int ListResize(List *list, const size_t length);
-void ListSet(List *list, const size_t index, const void *element);
-void ListGet(const List *list, const size_t index, void *elementDest);
-int ListAdd(List *list, const void *element);
-void ListRemove(List *list, const size_t index);
-void ListPopEnd(List *list, void *elementDest);
-void ListPopFront(List *list, void *elementDest);
-void ListFree(List *list);
 
 #endif
--- Tests/Tests.c
+++ Tests/Tests.c
@@ -7,73 +7,6 @@
 size_t TestCount = 0;
 size_t TestsPassed = 0;
 
-void TestList()
-{
-    List list;
-    
-    if(!ListCreate(sizeof(size_t), 10, &list))
-        FAIL("Failed to create list")
-    else
-        PASS("Successfully created list")
-
-    size_t testDataCount = 10;
-    size_t testData[testDataCount];
-
-    int addSuccessful = 1;
-
-    for(size_t x = 0; x < testDataCount; x++)
-    {
-        testData[x] = (x * 129 / 3) % 61;
-        addSuccessful &= ListAdd(&list, testData + x);
-        printf("Added: %llu\n", testData[x]);
-    }
-
-    if(addSuccessful)
-        PASS("Successfully added elements to list")
-    else
-        FAIL("Failed to add elements to list")
-
-    size_t endValue;
-    ListPopEnd(&list, &endValue);
-
-    size_t frontValue;
-    ListPopFront(&list, &frontValue);
-    ListRemove(&list, 1);
-
-    if(list.Count == testDataCount - 3 && list.Offset == 1 && frontValue == testData[0] && endValue == (size_t)(testData[testDataCount - 1]))
-        PASS("Successfully removed items from list")
-    else
-        FAIL("Failed to remove items from list")
-
-    if(!ListResize(&list, testDataCount - 3) || list.Length != testDataCount - 3 || !ListResize(&list, testDataCount + 50) || list.Length != testDataCount + 50)
-        FAIL("Failed to resize list")
-    else
-        PASS("Successfully resized list")
-
-    int getSuccessful = 1;
-
-    for(size_t x = 0, index = 0; x < testDataCount; x++, index++)
-    {
-        size_t value;
-
-        if(x == 0 || x == 2 || x == testDataCount - 1)
-        {
-            index --;
-            continue;
-        }
-
-        ListGet(&list, index, &value);
-        getSuccessful &= value == testData[x];
-
-        printf("Value: %llu\n", value);
-    }
-
-    if(getSuccessful)
-        PASS("Successfully read data from list\n")
-    else
-        FAIL("Failed to read data from list\n")
-}
-
 void TestCArray()
 {
     const size_t startingCArrayLength = 10;
@@ -89,8 +22,6 @@ void TestCArray()
         testData[x] = (x * 129 / 3) % 61;
         CArraySet(cArray, cArrayLength, cArrayOffset, sizeof(size_t), cArrayCount, testData + x);
         cArrayCount++;
-        //CArrayInsert(cArray, &cArrayCount, cArrayLength, &cArrayOffset, sizeof(size_t), cArrayCount, testData + x);
-        printf("Added: %llu\n", testData[x]);
     }
 
     CArrayRemove(cArray, &cArrayCount, cArrayLength, &cArrayOffset, sizeof(size_t), 0);
@@ -100,9 +31,6 @@ void TestCArray()
     CArrayInsert(cArray, &cArrayCount, cArrayLength, &cArrayOffset, sizeof(size_t), 1, &value);
     CArrayRemove(cArray, &cArrayCount, cArrayLength, &cArrayOffset, sizeof(size_t), 2);
     CArrayRemove(cArray, &cArrayCount, cArrayLength, &cArrayOffset, sizeof(size_t), cArrayCount);
-
-
-    printf("Count: %llu\n", cArrayCount);
 
     if(cArrayCount == cArrayLength - 3 && cArrayOffset == 2)
         PASS("Successfully removed items from CArray")
@@ -128,8 +56,6 @@ void TestCArray()
 
         CArrayGet(cArray, cArrayLength, cArrayOffset, sizeof(size_t), index, &value);
         getSuccessful &= value == testData[x];
-
-        printf("Value: %llu\n", value);
     }
 
     if(getSuccessful)
@@ -140,7 +66,6 @@ void TestCArray()
 
 int main (int argCount, char **argValues)
 {
-    TestList();
     TestCArray();
 
     printf("%llu out of %llu tests passed\n", TestsPassed, TestCount);
